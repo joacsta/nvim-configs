fa2bda519b6185a3a8ea9996887d51acd0c492ee
João Costa <jvcosta1502@gmail.com>
2026-04-15 20:46:41 -0300

    adicao de features

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,11 +1,13 @@
 return {
-    {
-        "folke/which-key.nvim",
-        opts = {
-            spec = {
-                { "<leader>M", desc = "Main Menu", icon = { icon = "󰍜 ", color = "cyan" } },
-                { "<leader>P", desc = "Netrw", icon = { icon = "󰕲 ", color = "yellow" } },
-            },
-        },
-    },
+	{
+		"folke/which-key.nvim",
+		opts = {
+			spec = {
+				{ "<leader>M", desc = "Main Menu", icon = { icon = "󰍜 ", color = "cyan" } },
+				{ "<leader>P", desc = "Netrw", icon = { icon = "󰕲 ", color = "yellow" } },
+				{ "<leader>A", desc = "Select all", icon = { icon = "󰒆 ", color = "blue" } },
+				{ "<leader>K", desc = "Keywordprg", icon = { icon = " ", color = "green" } },
+			},
+		},
+	},
 }
